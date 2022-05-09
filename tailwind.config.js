90c4209f98afdfac5f2e440ffa626f92f4203ec4
parkchangeon <qkrcksrjs@gmail.com>
2022-05-10 00:13:41 +0900

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: ["./pages/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      borderColor: theme => ({
+        "signature-color": "#7144FF",
+        "placeholder-color": "#e0e0e0",
+      }),
+      backgroundColor: theme => ({
+        "signature-color": "#7144FF",
+        "black-rgba": "rgba(0, 0, 0, 0.54)",
+        "placeholder-color": "#e0e0e0",
+      }),
+      textColor: theme => ({
+        "signature-color": "#7144FF",
+        "placeholder-color": "#e0e0e0",
+      }),
+      animation: {
+        slideUp: "slideUp 0.25s ease-out forwards",
+        slideDown: "slideDown 0.25s ease-out forwards",
+      },
+      keyframes: {
+        slideUp: {
+          "0%": {
+            transform: "translateY(200px)",
+          },
+          "100%": {
+            transform: "translateY(0px)",
+          },
+        },
+        slideDown: {
+          "0%": {
+            transform: "translateY(0px)",
+          },
+          "100%": {
+            transform: "translateY(200px)",
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
