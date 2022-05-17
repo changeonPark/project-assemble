5170dd7f3852594d1afd937bf0c4ed0b1cf335fa
parkchangeon <qkrcksrjs@gmail.com>
2022-05-17 23:25:45 +0900

    50% mobile header

--- src/base/components/NavBar.tsx
+++ src/base/components/NavBar.tsx
@@ -1,29 +1,51 @@
-import { cls } from "@base/utils"
+import { useState } from "react"
 import Link from "next/link"
+import { cls } from "@base/utils"
+
+import { IconClose, IconMenu } from "@base/icons"
 import UserInfo from "./UserInfo"
 
 const NavBar = () => {
+  const [toggle, setToggle] = useState<boolean>(false)
+
+  const onClickBtn = () => {
+    setToggle(prev => !prev)
+  }
+
   return (
-    <nav className="header-nav">
-      <ul
+    <>
+      <button className="block lg:hidden" onClick={() => onClickBtn()}>
+        {toggle ? <IconClose /> : <IconMenu />}
+      </button>
+
+      <nav
         className={cls(
-          "font-AeonikL",
-          "lg:flex lg:items-center lg:justify-center lg:gap-5 lg:text-sm"
+          toggle
+            ? "fixed top-20 left-0 z-30 block h-screen w-screen bg-[#fdfdfd] bg-opacity-70"
+            : "hidden",
+          "lg:static lg:block lg:h-fit lg:w-fit"
         )}
       >
-        <li>About</li>
-        <li>Gallery</li>
-        <li>Customizing</li>
-        <li>Puzzle</li>
-        <li>
-          <Link href="/mint">
-            <a>Mint</a>
-          </Link>
-        </li>
-        <li>Buy</li>
-        <UserInfo />
-      </ul>
-    </nav>
+        <ul
+          className={cls(
+            "flex flex-col items-center justify-between font-AeonikL",
+            "lg:flex-row lg:justify-center lg:gap-5 lg:text-sm"
+          )}
+        >
+          <li>About</li>
+          <li>Gallery</li>
+          <li>Customizing</li>
+          <li>Puzzle</li>
+          <li>
+            <Link href="/mint">
+              <a>Mint</a>
+            </Link>
+          </li>
+          <li>Buy</li>
+          <UserInfo />
+        </ul>
+      </nav>
+    </>
   )
 }
 
---
+++ src/base/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header"
+export { default as Logo } from "./Logo"
+export { default as NavBar } from "./NavBar"
+export { default as UserInfo } from "./UserInfo"
